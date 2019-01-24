c3636ae4de7a9c6c393abcc57520ed831625643d
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-01-24 16:34:09 +0100

    persistence-provider-api 0.10-SNAPSHOT.

--- src/main/java/no/ssb/lds/core/persistence/memory/MemoryPersistenceIntegrationTest.java
+++ src/main/java/no/ssb/lds/core/persistence/memory/MemoryPersistenceIntegrationTest.java
@@ -15,9 +15,9 @@ public class MemoryPersistenceIntegrationTest extends PersistenceIntegrationTest
 
     @BeforeClass
     public void setup() {
-        this.persistence = new MemoryInitializer().initialize(namespace,
-                Map.of("persistence.mem.wait.min", "0",
-                        "persistence.mem.wait.max", "0"),
+        persistence = new MemoryInitializer().initialize(namespace,
+                Map.of("persistence.fragment.capacity", "1024",
+                        "persistence.mem.transaction.cancel-on-close", "true"),
                 Set.of("Person", "Address", "FunkyLongAddress"));
     }
 
