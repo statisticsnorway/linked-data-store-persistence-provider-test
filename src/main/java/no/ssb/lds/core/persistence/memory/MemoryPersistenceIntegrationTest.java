6fe8b3e293c49d1aa2d54d47d3e1e45bdd92f8a9
Hadrien Kohl <hadrien.kohl@gmail.com>
2019-01-30 17:08:11 +0100

    Remove bridge

--- src/main/java/no/ssb/lds/core/persistence/memory/MemoryPersistenceIntegrationTest.java
+++ src/main/java/no/ssb/lds/core/persistence/memory/MemoryPersistenceIntegrationTest.java
@@ -1,17 +1,11 @@
 package no.ssb.lds.core.persistence.memory;
 
-import no.ssb.lds.api.persistence.json.JsonPersistence;
-import no.ssb.lds.api.persistence.reactivex.RxJsonPersistenceBridge;
-import no.ssb.lds.api.persistence.reactivex.RxPersistenceBridge;
-import no.ssb.lds.api.persistence.streaming.Persistence;
 import no.ssb.lds.core.persistence.test.PersistenceIntegrationTest;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.util.Map;
 import java.util.Set;
-
-import static java.util.Optional.ofNullable;
 
 public class MemoryPersistenceIntegrationTest extends PersistenceIntegrationTest {
 
@@ -21,12 +15,10 @@ public class MemoryPersistenceIntegrationTest extends PersistenceIntegrationTest
 
     @BeforeClass
     public void setup() {
-        JsonPersistence memoryJsonPersistence = new MemoryInitializer().initialize(namespace,
+        this.persistence = new MemoryInitializer().initialize(namespace,
                 Map.of("persistence.mem.wait.min", "0",
                         "persistence.mem.wait.max", "0"),
                 Set.of("Person", "Address", "FunkyLongAddress"));
-        Persistence memoryPersistence = memoryJsonPersistence.getPersistence();
-        this.persistence = new RxJsonPersistenceBridge(new RxPersistenceBridge(memoryPersistence),8192);
     }
 
     @AfterClass
